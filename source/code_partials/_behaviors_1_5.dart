093405f0e13a63003432c4d28a33bcf5f9b479d8
Roman Snitko <roman.snitko@gmail.com>
2016-07-31 11:24:36 +0700

    Documentation for the positioning feature

---
+++ source/code_partials/_behaviors_1_5.dart
@@ -0,0 +1,5 @@
+// assuming we are inside a Behavior class function somewhere:
+
+var green = querySelector(".greenBlock");
+var blue  = querySelector(".blueBlock");
+pos.placeAboveTopLeft(blue, green);
---
+++ source/code_partials/_behaviors_1_6.dart
@@ -0,0 +1,5 @@
+placeBy(
+  HtmlElement el1,
+  HtmlElement el2,
+  { double left: 0, double top: 0, double gravity_top: 0, double gravity_left: 0 }
+)
